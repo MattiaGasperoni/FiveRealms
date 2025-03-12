b3ad1de825684d558d9e8e02eb60d8dd87e38374
Mattia Gasperoni <mattiagasperoni02@gmail.com>
2025-03-12 18:17:17 +0100

    Modifications on the Game and Level classes, and created the checkAndReplaceDeadAllies method

--- src/model/gameStatus/Game.java
+++ src/model/gameStatus/Game.java
@@ -3,6 +3,7 @@ package model.gameStatus;
 import java.util.ArrayList;
 import java.util.List;
 
+import controller.CombatManager;
 import model.characters.AbstractCharacter;
 import model.characters.Archer;
 import model.characters.Barbarian;
@@ -10,73 +11,64 @@ import view.LevelMap;
 
 public class Game 
 {   
-	private static final int NUMLEVELS = 5;
-
+    private static final int NUMLEVELS = 5;
+    private static final int NUMALLIES = 3;
     private List<Level> levelList;    
-	private List<AbstractCharacter> currentAllies;
-	
-	
+    private List<AbstractCharacter> alliesList;
+    
     private EnemyManager enemyManager;
+    private CombatManager combatManager;
 
-	private boolean tutorialCompleted;
+    private boolean tutorialCompleted;
 
-	public Game() 
-	{
-		this.levelList         = new ArrayList<>();
-		this.currentAllies     = new ArrayList<>();
-		this.enemyManager      = new EnemyManager();
-		this.tutorialCompleted = false;
-		initializeLevels();
-		startGame();
-	}
-
-	private void initializeLevels() 
-	{
-		// Inizializziamo la lista dei livelli con il tutorial e i livelli principali
-
-        // Creo e aggiungo il tutorial
-        this.levelList.add(new Level(new LevelMap(), generateTutorialEnemies(), this.currentAllies));
-
-		// Creo i livelli principali 
-		List<AbstractCharacter> level1Enemies = new ArrayList<>();
-		level1Enemies.add(new Barbarian());
-		level1Enemies.add(new Archer());
-
-		List<AbstractCharacter> level2Enemies = new ArrayList<>();
-		level2Enemies.add(new Barbarian());
-		level2Enemies.add(new Archer());
-
-		List<AbstractCharacter> level3Enemies = new ArrayList<>();
-		level3Enemies.add(new Barbarian());
-		level3Enemies.add(new Archer());
-
-		List<AbstractCharacter> level4Enemies = new ArrayList<>();
-		level4Enemies.add(new Barbarian());
-		level4Enemies.add(new Archer());
-
-		List<AbstractCharacter> level5Enemies = new ArrayList<>();
-		level5Enemies.add(new Barbarian());
-		level5Enemies.add(new Archer());
-
-		//aggiungo tutti i livelli alla lista
-		this.levelList.add(new Level(new LevelMap(), level1Enemies, this.currentAllies));
-		this.levelList.add(new Level(new LevelMap(), level2Enemies, this.currentAllies));
-		this.levelList.add(new Level(new LevelMap(), level3Enemies, this.currentAllies));
-		this.levelList.add(new Level(new LevelMap(), level4Enemies, this.currentAllies));
-		this.levelList.add(new Level(new LevelMap(), level5Enemies, this.currentAllies));
+    public Game() 
+    {
+        this.levelList         = new ArrayList<>();
+        this.alliesList        = new ArrayList<>();
+        this.enemyManager      = new EnemyManager();
+        this.combatManager     = new CombatManager();
+        this.tutorialCompleted = false;
+        initializeLevels();
+        startGame();
     }
 
-    private List<AbstractCharacter> generateTutorialEnemies() 
-	{
-		// Creo la lista dei nemici del tutorial
+    private void initializeLevels() 
+    {
+        // Inizializziamo la lista dei livelli con il tutorial e i livelli principali
+
+        // Creo e aggiungo il tutorial
         List<AbstractCharacter> tutorialEnemies = new ArrayList<>();
-        
-		// Popolo la lista con i nemici
-		tutorialEnemies.add(new Barbarian());
+        tutorialEnemies.add(new Barbarian());
         tutorialEnemies.add(new Archer());
 
-        // Aggiungi altri nemici specifici per il tutorial
-        return tutorialEnemies;
+        // Creo i livelli principali 
+        List<AbstractCharacter> level1Enemies = new ArrayList<>();
+        level1Enemies.add(new Barbarian());
+        level1Enemies.add(new Archer());
+
+        List<AbstractCharacter> level2Enemies = new ArrayList<>();
+        level2Enemies.add(new Barbarian());
+        level2Enemies.add(new Archer());
+
+        List<AbstractCharacter> level3Enemies = new ArrayList<>();
+        level3Enemies.add(new Barbarian());
+        level3Enemies.add(new Archer());
+
+        List<AbstractCharacter> level4Enemies = new ArrayList<>();
+        level4Enemies.add(new Barbarian());
+        level4Enemies.add(new Archer());
+
+        List<AbstractCharacter> level5Enemies = new ArrayList<>();
+        level5Enemies.add(new Barbarian());
+        level5Enemies.add(new Archer());
+
+        //aggiungo tutti i livelli alla lista
+        this.levelList.add(new Level(new LevelMap(), tutorialEnemies, this.alliesList));
+        this.levelList.add(new Level(new LevelMap(), level1Enemies, this.alliesList));
+        this.levelList.add(new Level(new LevelMap(), level2Enemies, this.alliesList));
+        this.levelList.add(new Level(new LevelMap(), level3Enemies, this.alliesList));
+        this.levelList.add(new Level(new LevelMap(), level4Enemies, this.alliesList));
+        this.levelList.add(new Level(new LevelMap(), level5Enemies, this.alliesList));
     }
 
     public List<Level> getLevelList() {
@@ -84,51 +76,32 @@ public class Game
     }
 
     public void startGame() 
-	{
-		// Da fare il controllo se hai cliccato il bottone per saltare il tutorial 
-		
+    {
+        // Da fare il controllo se hai cliccato il bottone per saltare il tutorial 
+        
         if (!tutorialCompleted) 
-		{
-			// Gioca il tutorial
-            playLevel(levelList.get(0)); 
-            this.tutorialCompleted = true;
+        {
+            // Gioca il tutorial
+           	this.tutorialCompleted = levelList.get(0).playLevel(enemyManager, combatManager);
         }
 
         for (int i = 1; i < levelList.size(); i++) 
-		{
-            playLevel(levelList.get(i));
+        {
+            if(levelList.get(i).playLevel(enemyManager, combatManager))
+            	checkAndReplaceDeadAllies();
         }
     }
 
-    private void playLevel(Level level) 
-	{
-		// Spawn dei nemici
-        enemyManager.spawnEnemies(level.getEnemies());
-
-        while (!enemyManager.allEnemiesDefeated()) 
+    private void checkAndReplaceDeadAllies() 
+    {
+		// Rimuovi alleati morti
+		this.alliesList.removeIf(ally -> !ally.isAlive());
+		
+		// Aggiungi nuovi alleati se necessario
+		while (this.alliesList.size() < NUMALLIES) 
 		{
-            // Logica del combattimento
-        }
-		// Despawn dei nemici
-        enemyManager.despawnEnemies();
-
-		// Aggiorna la lista degli alleati
-        currentAllies = updateAllies(level.getAllies());
-        level.setAllies(currentAllies);
-    }
-
-    private List<AbstractCharacter> updateAllies(List<AbstractCharacter> allies) 
-	{
-        List<AbstractCharacter> updatedAllies = new ArrayList<>();
-        for (AbstractCharacter ally : allies) 
-		{
-			// Da fare metodo per capire se un personaggio e' ancora giocabile
-            if (ally.isAlive()) 
-			{
-                updatedAllies.add(ally);
-            }
-        }
-        return updatedAllies;
+			this.alliesList.add(new Archer()); // Aggiungi un nuovo alleato scelto dall'utente
+		}
     }
 
     public void skipTutorial() {
--- src/model/gameStatus/Level.java
+++ src/model/gameStatus/Level.java
@@ -1,35 +1,73 @@
 package model.gameStatus;
 
-import view.LevelMap;
+import java.util.ArrayList;
 import java.util.List;
 
+import controller.CombatManager;
 import model.characters.AbstractCharacter;
+import view.LevelMap;
 
-public class Level {
+public class Level 
+{
+    private final LevelMap LevelMap;
+    private List<AbstractCharacter> enemiesList;
+    private List<AbstractCharacter> alliesList;
+    private boolean doorIsOpen;
 
-    private LevelMap levelmap;
-    private List<AbstractCharacter> enemies;
-    private List<AbstractCharacter> allies;
-
-    public Level(LevelMap levelmap, List<AbstractCharacter> enemies, List<AbstractCharacter> allies) {
-        this.levelmap = levelmap;
-        this.enemies = enemies;
-        this.allies = allies;
-    }
-
-    public LevelMap getLevelMap() {
-        return  this.levelmap;
+    public Level(LevelMap map, List<AbstractCharacter> enemies, List<AbstractCharacter> allies) 
+    {
+        this.LevelMap     = map;
+        this.enemiesList  = enemies;
+        this.alliesList   = allies;
+        this.doorIsOpen   = false;
     }
 
     public List<AbstractCharacter> getEnemies() {
-        return  this.enemies;
+        return this.enemiesList;
     }
 
     public List<AbstractCharacter> getAllies() {
-        return  this.allies;
+        return this.alliesList;
     }
 
-    public void setAllies(List<AbstractCharacter> allies) {
-        this.allies = allies;
+    //Metodo Pubblico per giocare il livello
+    public boolean playLevel(EnemyManager enemyManager, CombatManager combatManager) {
+        
+        // Spawn dei nemici
+        enemyManager.spawnEnemies(this.enemiesList);
+
+        // Metodo per lo spawn degli alleati
+        // ...
+
+        // Il livello continua finche non apri la porta per il prossimo livello o muoiono tutti i tuoi personaggi 
+        while (true) // allAlliesDead or this.doorIsOpen == false
+        {
+            // INIZIO FASE DI ATTACCO
+
+            // con il combatManager se un nemico o un alleate viene ucciso in quella fase di attacco 
+            // viene tolto dalla mappa e rimosso dalla lista
+
+            // Logica del combattimento
+            // ...
+
+            // Controlla se tutti gli alleati sono morti
+            if (false)  // this.alliesDeaths == 3  
+            {
+                System.out.println("Tutti i tuoi personaggi sono morti. Game Over.");
+                return false;
+            }
+
+            // FINE FASE DI ATTACCO
+
+            // Controlla se tutti i nemici sono stati sconfitti
+            if (enemyManager.allEnemiesDefeated()) 
+            {
+                // Attiva la porta per il prossimo livello
+                this.doorIsOpen = true;
+                System.out.println("Hai sconfitto tutti i nemici. La porta per il prossimo livello e' aperta.");
+            }
+        }
+
+        return true;
     }
 }
